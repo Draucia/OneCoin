deb74b17d17343cc3f714ff11adba211f620f4fe
Draucia <zhinkk@gmail.com>
2013-11-30 18:17:07 -0500

    Update main.h
    - Changed MIN_TX_FEE = 0
    - Changed MAX_MONEY = 1 * COIN

--- src/main.h
+++ src/main.h
@@ -38,9 +38,9 @@ static const int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const int64 COIN = 100000000;
 static const int64 CENT = 1000000;
-static const int64 MIN_TX_FEE = 10000000; // Litecoin: minimum transaction fee of 0.1 LTC
+static const int64 MIN_TX_FEE = 0; // Litecoin: minimum transaction fee of 0.1 LTC
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE; // Litecoin: minimum relay transaction fee same as minimum transaction fee.
-static const int64 MAX_MONEY = 84000000 * COIN; // Litecoin: maximum of 840k coins
+static const int64 MAX_MONEY = 1 * COIN; // Litecoin: maximum of 840k coins
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 static const int COINBASE_MATURITY = 100;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
